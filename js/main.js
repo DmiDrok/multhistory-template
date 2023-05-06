d5a18a26a27ff564d3d24cf8a5874d7e4f8d2349
DmiDrok <drobkov155099@gmail.com>
2023-05-06 16:42:19 +0300

    add webp

--- js/main.js
+++ js/main.js
@@ -12,6 +12,9 @@ document.addEventListener('DOMContentLoaded', function () {
     console.error(err);
   }
 });
+window.addEventListener('resize', function () {
+  location.reload();
+});
 
 // Слайдеры
 function setCorrectSliders() {
@@ -74,7 +77,7 @@ function setCorrectSliders() {
         bulletActiveClass: 'pagination-bullet_active'
       }
     });
-    if (window.matchMedia('(min-width: 720px)').matches) {
+    if (window.matchMedia('(min-width: 880px)').matches) {
       reviewsSwiper.destroy();
     }
   };
